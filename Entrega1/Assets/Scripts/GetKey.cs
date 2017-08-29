1314f4ef28f2809f3e5b124afeb5f13ff9441561
Javi <jajayoda@hotmail.com>
2017-08-28 21:36:24 -0300

    no message

---
+++ Entrega1/Assets/Scripts/GetKey.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetKey : MonoBehaviour
+{
+    private Animator anim;
+    public bool Run;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetBool("Run", true);
+        }
+        anim.SetBool("Run", false);
+    }
+}
